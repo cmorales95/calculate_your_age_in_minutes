1fd0c23e86b30935c6c1182533507217cb2a4106
Cristian Morales <cristian.morales@rappi.com>
2021-08-03 20:28:30 -0500

    finishing app and changing to age in days

--- app/src/main/java/com/cmorales/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/cmorales/ageinminutes/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -24,9 +25,32 @@ class MainActivity : AppCompatActivity() {
         val year = myCalendar.get(Calendar.YEAR)
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
-        DatePickerDialog(this, DatePickerDialog.OnDateSetListener {
-                view, year, month, dayOfMonth ->
-            Toast.makeText(this, "Date Picker works", Toast.LENGTH_LONG)
-        }, year, month, day).show()
+
+        val dpd = DatePickerDialog(this,
+            { view, selectedYear, selectedMonth, selectedDay ->
+
+                val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                tvSelectedDate.text = selectedDate
+
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+
+                val theDate = sdf.parse(selectedDate)
+
+                val selectedDateInMinutes = theDate.time / 86400000
+
+                val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+
+                val currentDateToMinutes = currentDate.time  / 86400000
+
+                val differenceInMinutes = currentDateToMinutes - selectedDateInMinutes
+
+                tvSelectedDateInMinutes.text = differenceInMinutes.toString()
+
+            }, year, month, day)
+
+        // block dates in the future
+        dpd.datePicker.maxDate = Date().time - 86400000
+        dpd.show()
+
     }
 }
